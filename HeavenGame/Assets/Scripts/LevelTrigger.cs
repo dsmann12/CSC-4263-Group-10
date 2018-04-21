53a482e7cd02bdb3efb5b0a6fc4832ae09c6a8f4
David Scheuermann <dsmann1212@gmail.com>
2018-04-20 21:33:35 -0500

    Added colliders and some enemies to levels. Added level triggers

---
+++ HeavenGame/Assets/Scripts/LevelTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelTrigger : MonoBehaviour {
+    public string level;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            LevelManager lm = new LevelManager();
+            lm.LoadLevel(level);
+        }
+    }
+}
